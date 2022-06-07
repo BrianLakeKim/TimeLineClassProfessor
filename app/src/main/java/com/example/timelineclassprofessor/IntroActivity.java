acef447a2a8357ad79d4c3c5c954cf74386e003a
BrianLake <whddhr521@naver.com>
2022-06-07 16:58:41 +0900

    Initial commit

---
+++ app/src/main/java/com/example/timelineclassprofessor/IntroActivity.java
@@ -0,0 +1,28 @@
+package com.example.timelineclassprofessor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        handler = new Handler();
+
+        new Handler() .postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(IntroActivity.this, login.class);
+                startActivity(intent);
+            }
+        }, 1500);
+    }
+}
