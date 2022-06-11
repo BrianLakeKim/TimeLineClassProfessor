ceb510e7f151ee57faf7b2ea21251eb7fa8b6cc1
BrianLake <crwate521@gmail.com>
2022-06-11 15:52:50 +0900

    교수자용 어플 : 메인화면 보기방식 2가지 추가

---
+++ app/src/main/java/com/example/timelineclassprofessor/MainActivitylistview.java
@@ -0,0 +1,14 @@
+package com.example.timelineclassprofessor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivitylistview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activitylistview);
+    }
+}
